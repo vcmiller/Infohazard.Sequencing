0781ca8d08c3f11192246e91c51e8f539bfb6d70
Vincent Miller <vmiller0725@gmail.com>
2024-07-07 17:00:09 -0700

    Loading screen can show a button prompt on completion.

--- Runtime/Sequencing/LoadingScreen.cs
+++ Runtime/Sequencing/LoadingScreen.cs
@@ -1,17 +1,17 @@
 // The MIT License (MIT)
-// 
+//
 // Copyright (c) 2022-present Vincent Miller
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -31,8 +31,19 @@ namespace Infohazard.Sequencing {
     public class LoadingScreen : Singleton<LoadingScreen> {
         [SerializeField] private ProgressBar _progressBar;
         [SerializeField] private TMP_Text _actionText;
+        [SerializeField] private bool _switchToButtonPromptOnComplete;
+
+        [SerializeField, ConditionalDraw(nameof(_switchToButtonPromptOnComplete))]
+        private GameObject _buttonPrompt;
+
+        [SerializeField] private GameObject _loadingBarParent;
 
         private IProgressSource _progressSource;
+
+        public bool SwitchToButtonPromptOnComplete {
+            get => _switchToButtonPromptOnComplete;
+            set => _switchToButtonPromptOnComplete = value;
+        }
 
         public void SetText(string text) {
             _actionText.text = text;
@@ -41,6 +52,19 @@ namespace Infohazard.Sequencing {
         public void SetProgress(float progress) {
             _progressSource = null;
             _progressBar.FillAmount = progress;
+
+            UpdateCompletionState(progress);
+        }
+
+        private void UpdateCompletionState(float progress) {
+            bool isComplete = progress >= 1 && _switchToButtonPromptOnComplete;
+            if (_buttonPrompt != null) {
+                _buttonPrompt.SetActive(isComplete);
+            }
+
+            if (_loadingBarParent) {
+                _loadingBarParent.SetActive(!isComplete);
+            }
         }
 
         public void SetProgressSource(IProgressSource progressSource) {
@@ -53,9 +77,10 @@ namespace Infohazard.Sequencing {
         }
 
         private void UpdateFromProgressSource() {
-            if (_progressSource != null) {
-                _progressBar.FillAmount = _progressSource.Progress;
-            }
+            if (_progressSource == null) return;
+            float progress = _progressSource.Progress;
+            _progressBar.FillAmount = progress;
+            UpdateCompletionState(progress);
         }
     }
 }
