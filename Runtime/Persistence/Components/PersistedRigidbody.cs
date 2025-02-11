f1fcc192ad2799df259d6b8972d51d2dfbe9c22f
Vincent Miller <vmiller0725@gmail.com>
2025-02-10 23:57:05 -0800

    Unity 6 fix

--- Runtime/Persistence/Components/PersistedRigidbody.cs
+++ Runtime/Persistence/Components/PersistedRigidbody.cs
@@ -1,17 +1,17 @@
 ﻿// The MIT License (MIT)
-// 
+//
 // Copyright (c) 2022-present Vincent Miller
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,7 +21,7 @@
 // SOFTWARE.
 
 using System;
-
+using Infohazard.Core;
 using UnityEngine;
 
 namespace Infohazard.Sequencing {
@@ -32,7 +32,7 @@ namespace Infohazard.Sequencing {
 
         public override void LoadState() {
             base.LoadState();
-            if (_saveVelocity) _rigidbody.velocity = State.Velocity;
+            if (_saveVelocity) _rigidbody.SetLinearVelocity(State.Velocity);
             if (_saveAngularVelocity) _rigidbody.angularVelocity = State.AngularVelocity;
         }
 
@@ -42,7 +42,7 @@ namespace Infohazard.Sequencing {
         }
 
         private void SaveState() {
-            if (_saveVelocity) State.Velocity = _rigidbody.velocity;
+            if (_saveVelocity) State.Velocity = _rigidbody.GetLinearVelocity();
             if (_saveAngularVelocity) State.AngularVelocity = _rigidbody.angularVelocity;
         }
 
